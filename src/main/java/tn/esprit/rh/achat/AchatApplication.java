6aaa04478956d8af771dbb9e705394ee74992653
Zeroxcharisma <79290030+Zeroxcharisma@users.noreply.github.com>
2022-10-17 23:51:26 +0100

    Update AchatApplication.java

--- src/main/java/tn/esprit/rh/achat/AchatApplication.java
+++ src/main/java/tn/esprit/rh/achat/AchatApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 @EnableScheduling
 @SpringBootApplication
 public class AchatApplication {
