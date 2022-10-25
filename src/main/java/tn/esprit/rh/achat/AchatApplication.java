8db39956bf40fa6d9bf723ceef51c92088e07673
Zeroxcharisma <79290030+Zeroxcharisma@users.noreply.github.com>
2022-10-25 11:01:55 +0100

    Update AchatApplication.java

--- src/main/java/tn/esprit/rh/achat/AchatApplication.java
+++ src/main/java/tn/esprit/rh/achat/AchatApplication.java
@@ -2,13 +2,10 @@ package tn.esprit.rh.achat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
-@ComponentScan({"tn.esprit.rh.achat.repositories"})
-@EnableJpaRepositories("tn.esprit.rh.achat.repositories")
+
 @EnableScheduling
 @SpringBootApplication
 public class AchatApplication {
